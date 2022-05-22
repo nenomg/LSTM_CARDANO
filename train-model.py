8fc7fb9baf25e7901df55cf25a50681b7b023373
eugenio menacho de góngora <eugeniomenachodegongora@gmail.com>
2022-05-22 21:13:25 +0200

    Actualizacion script

--- train-model.py
+++ train-model.py
@@ -26,22 +26,25 @@ def getY(data):
 
 
 #We read the dataset
-data = pd.read_csv("ADAUSDT.csv",low_memory=False, names = ["Date","Open","High", "Low", "Close", "Vol","quote_asset_volume", "num_trades", "taker_buy_base_asset_volume", "taker_buy_quote_asset_volume"])
+data = pd.read_csv("ADAUSDT.csv",low_memory=False, names = ["Date","Open",
+"High", "Low", "Close", "Vol","quote_asset_volume", "num_trades", 
+"taker_buy_base_asset_volume", "taker_buy_quote_asset_volume"])
+
 data = data[1::]
 data = data.reset_index(drop=True)
 
-#We get the lastests dates
-data_lastests_dates = data.Date[len(data)-201:len(data)].reset_index(drop=True)
+
+#Drop unnecesary columns
+data = data.drop(['Date','quote_asset_volume', 'taker_buy_base_asset_volume', 
+                  'Open',"taker_buy_quote_asset_volume"], axis = 1)
 
 #Change data to numeric
-data[["Open","High", "Low", "Close", "Vol","quote_asset_volume", "num_trades", "taker_buy_base_asset_volume", "taker_buy_quote_asset_volume"]] = data[["Open","High", "Low", "Close", "Vol","quote_asset_volume", "num_trades", "taker_buy_base_asset_volume", "taker_buy_quote_asset_volume"]].apply(pd.to_numeric)
+data = data.apply(pd.to_numeric)
 
 
 #Get the data for y, in this case the next row high value
 y_aux = getY(data)
 
-#Drop unnecesary columns
-data = data.drop(['Date','quote_asset_volume', 'taker_buy_base_asset_volume', 'Open',"taker_buy_quote_asset_volume"], axis = 1)
 
 #Set the scaler
 scaler = MinMaxScaler()
@@ -71,7 +74,8 @@ y_test = np.array(y[len(y)-200:len(y)])
 
 #Define the tensorflow model
 model = Sequential() 
-model.add(LSTM(units = 20, activation = 'relu', return_sequences = True, input_shape = (X_train.shape[1], 5)))
+model.add(LSTM(units = 20, activation = 'relu', return_sequences = True, 
+               input_shape = (X_train.shape[1], 5)))
 
 model.add(LSTM(units = 30, activation = 'relu', return_sequences = True))
 
@@ -86,7 +90,8 @@ model.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
 #Train the model
 iter = 1000
-history= model.fit(X_train, y_train, epochs = iter, batch_size =70, validation_split=0.00001)
+history= model.fit(X_train, y_train, epochs = iter, batch_size =70, 
+                   validation_split=0.00001)
 
 #Predict the test values
 predictions = model.predict(X_test)
